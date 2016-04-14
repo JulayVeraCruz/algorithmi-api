2a15b13d4642b3d73d7ca5c9fc251ab38ab7ba6d
FRosa3560 <filipenrosa@sapo.pt>
2016-04-14 21:57:11 +0100

    jason->sql Filipe

--- src/main/java/algorithmi/models/Course.java
+++ src/main/java/algorithmi/models/Course.java
@@ -15,6 +15,14 @@
  */
 package algorithmi.models;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author FilipeRosa
@@ -23,13 +31,37 @@ public class Course {
     private int codCourse;
     private String name;
     private int school;
+    
+    public Course(String data) {
 
+        //Transforma a string recebida pelo pedido http para json
+        JsonParser jsonParser = new JsonParser();
+        JsonObject Course = (JsonObject) jsonParser.parse(data);
+        //Exibe os dados, em formato json
+        System.out.println(Course.entrySet());
+        /**
+         *
+         * Revalidar TUDO, formatos, campos vazios, TUDO!!
+         *
+         */
+        validateData();
+        //Associa os dados ao objecto Course
+        this.codCourse = Course.getAsInt(); //ir buscar o max id da bd + 1 
+        this.name = Course.get("name").getAsString();
+        this.school=Course.get("school").getAsInt();
+       
+    }
+
+    public void regist() {
+        //Insere na BD
+    }
 
     public Course(int CodCurse, String name, int scholl) {
         this.codCourse = codCourse;
         this.name = name;
         this.school = scholl;
     }
+    
     public int getCodCourse() {
         return codCourse;
     }
@@ -55,7 +87,25 @@ public class Course {
     }
     
     
-    
-            
+    // converts a java object to JSON format,
+    // and returned as JSON formatted string
+    @Override
+    public String toString() {
+        Gson gson = new Gson();
+
+        String json = gson.toJson(this);
+        System.out.println("json \n" + json);
+        return json;
+    }
+
+    private void validateData() {
+        /**
+         * Se estiver tudo OK, inserer na BD,
+         */
+        regist();
+         /**Senão Devolve um erro (mas dos
+         * amigáveis :D)
+         */
+    }       
     
 }
